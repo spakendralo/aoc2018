d467ff16acc8f7d8bae3b31d4017dda42cb4fa89
spakendralo <spakendralo@gmail.com>
2018-12-01 22:59:19 +0100

    a

---
+++ src/main/java/lu/pistache/advent2018/Day1.java
@@ -0,0 +1,37 @@
+package lu.pistache.advent2018;
+
+import java.io.*;
+import java.util.*;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException {
+        Set freqs = new HashSet();
+        List<Integer> integers = readFile(); //assume we can keep all the file in memory
+        int result = 0;
+        boolean found = false;
+        while (!found) {
+            System.out.print(".");
+            for (Integer integer : integers) {
+                result += integer;
+                if (freqs.contains(result)) {
+                    System.out.println();
+                    System.out.println("2 : " + result);
+                    found = true;
+                    break;
+                } else {
+                    freqs.add(result);
+                }
+            }
+        }
+    }
+
+    private static List<Integer> readFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/day1-1.txt"));
+        String thisLine;
+        List<Integer> lines = new ArrayList<>();
+        while ((thisLine = br.readLine()) != null) {
+            lines.add(Integer.parseInt(thisLine));
+        }
+        return lines;
+    }
+}
---
+++ src/main/java/lu/pistache/advent2018/Day1MapReduce.java
@@ -0,0 +1,24 @@
+package lu.pistache.advent2018;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Day1MapReduce {
+    private Stream<String> stream;
+
+    public Day1MapReduce(Stream<String> stream) {
+        this.stream = stream;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Stream<String> stream = Files.lines(Paths.get("src/main/resources/day1-1.txt"));
+        Integer sum = stream.parallel().map(Integer::parseInt)
+                .reduce((integer, integer2) -> integer + integer2)
+                .get();
+        System.out.println("1 : " + sum);
+    }
+}
