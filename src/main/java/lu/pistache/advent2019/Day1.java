cec41d3e9f05695eab1623fb4f4914685b9cf346
spakendralo <spakendralo@gmail.com>
2019-12-04 22:55:05 +0100

    2019

--- src/main/java/lu/pistache/advent2019/Day1.java
+++ src/main/java/lu/pistache/advent2019/Day1.java
@@ -11,18 +11,44 @@ import java.util.stream.Stream;
 
 public class Day1 {
 
+    static int fuel = 0;
+
     public static void main(String[] args) throws IOException {
-        List<String> lines = Utils.loadLines("src/main/resources/advent2019/day1.txt");
-        int carb = 0;
-        for (String line : lines) {
-            carb += ((int) (Integer.parseInt(line) / 3)) - 2;
-        }
-        System.out.println(carb);
+
 
 
         Stream<String> strings = Files.lines(Paths.get("src/main/resources/advent2019/day1.txt"));
-        int s = strings.map(Integer::parseInt).map((x) -> ((int) x / 3) - 2).reduce((x, y) -> x + y).get();
-        System.out.println(s);
+        int s = strings.map(Integer::parseInt)
+                .map((x) -> calcul(x))
+                .reduce((x, y) -> x + y).get();
+        System.out.println("First part:" + s);
 
+
+        List<String> lines = Utils.loadLines("src/main/resources/advent2019/day1.txt");
+        lines.forEach(x -> calculateSingleFuel(Integer.parseInt(x)));
+        System.out.println("Second part:" + fuel);
+
+    }
+
+    private static void calculateSingleFuel(int weight) {
+        fuel += recursiveCalculate(weight);
+    }
+
+    private static int recursiveCalculate(int weight) {
+        int stepWeight = 0;
+        int fuelNeededForStep = calcul(weight);
+        if (fuelNeededForStep >= 9) {
+            stepWeight = recursiveCalculate(fuelNeededForStep);
+        }
+        else {
+            stepWeight = fuelNeededForStep;
+            return stepWeight;
+        }
+        stepWeight += fuelNeededForStep;
+        return stepWeight;
+    }
+
+    private static int calcul(int weight) {
+        return (weight / 3) - 2;
     }
 }
